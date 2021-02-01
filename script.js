a592496bd4e818b74315c70727b9551000fddf4f
Jose Valencia <sejoma989@gmail.com>
2021-02-01 17:28:07 -0500

    correccion de errores

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ const cellElements = document.querySelectorAll('[data-cell]')
 const board = document.getElementById('board')
     //Contiene los elementos del modal del mensaje ganador
 const winningMessageElement = document.getElementById('winningMessage')
+const restartButton = document.getElementById('restartButton')
     //Selecciona el selector usado en la linea 30 de index para definir el mensaje con el ganador, si es x o O
 const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
 
@@ -28,13 +29,19 @@ let circleTurn
 
 startGame()
 
+restartButton.addEventListener('click', startGame)
+
 //funcion que arranca el juego para que comience con setBoardHoverClass
 function startGame() {
     circleTurn = true
     cellElements.forEach(cell => {
+        cell.classList.remove(X_CLASS)
+        cell.classList.remove(CIRCLE_CLASS)
+        cell.removeEventListener('click', handleClick)
         cell.addEventListener('click', handleClick, { once: true }) //Escucha un solo evento de click en cada celda, solo uno
     })
     setBoardHoverClass()
+    winningMessageElement.classList.remove('show')
 }
 
 function handleClick(e) {
@@ -47,7 +54,6 @@ function handleClick(e) {
     } else if (isDraw()) { //comprobar Empate
         endGame(true)
     } else {
-        console.log("Estoy entrando aca")
         swapTurns() //cambiar Turno
         setBoardHoverClass() //Pone las marcas al hacer hover
     }
@@ -56,11 +62,17 @@ function handleClick(e) {
 
 function endGame(draw) {
     if (draw) {
-
+        winningMessageTextElement.innerText = "Empate!"
     } else {
-        winningMessageTextElement.innerHTML = `${circleTurn ? "O" : "X"} Gana!`
+        winningMessageTextElement.innerText = `${circleTurn ? "O" : "X"} Gana!`
     }
     winningMessageElement.classList.add('show')
+}
+
+function isDraw() {
+    return [...cellElements].every(cell => {
+        return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+    })
 }
 
 function placeMark(cell, currentClass) {
@@ -81,9 +93,6 @@ function setBoardHoverClass() {
     }
 }
 
-function isDraw() {
-
-}
 //Funcion que compara segun el turno para todos los array combinaciones para ganar, 
 //si alguna de ellas tiene todos los index de la misma clase
 function checkWin(currentClass) {
